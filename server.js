46c0ab48226ca4acfdf452d3543cd9b0524c4231
Zane Campbell <zanegitinit@gmail.com>
2019-06-23 20:36:42 -0700

    test 2 from home pc

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -7,10 +7,12 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
+app.get('/', (req, res) => res.send('API Running'));
 
-app.use("/api", require("./routes/api/poll"));
+app.use('/api', require('./routes/api/poll'));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Test commit from home pc
