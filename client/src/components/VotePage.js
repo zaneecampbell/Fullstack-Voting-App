c1315afa6680ae985c6330eef00c16de86647c2a
Zane Campbell <zanegitinit@gmail.com>
2019-07-12 12:12:51 -0700

    setup get call for VotePage

--- client/src/components/VotePage.js
+++ client/src/components/VotePage.js
@@ -1,33 +1,39 @@
 import React, { Fragment, useEffect, useState } from 'react';
+import { Button } from '@material-ui/core';
 import axios from 'axios';
 
 const VotePage = ({ match }) => {
-  const id = match.params.id;
-
   const [formData, setFormData] = useState(null);
 
   useEffect(() => {
     try {
-      const config = {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
+      const id = match.params.id;
+      const getData = async () => {
+        try {
+          const config = {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          };
 
-      const res = await axios.get(`/api/get/${id}`, config);
-      setFormData({
-        question: res.data.question,
-        options: res.data.options
-      });
-      console.log(formData);
+          const res = await axios.get(`/api/get/${id}`, config);
+          console.log(res);
+          setFormData({
+            question: res.data.question,
+            options: res.data.options
+          });
+        } catch (err) {}
+      };
+      getData();
     } catch (error) {
       console.log(error);
     }
-  }, [formData, id]);
+  }, [match]);
 
   return (
     <Fragment>
       <h1 style={{ fontSize: '100px' }}>hi</h1>
+      <Button onClick={() => console.log(formData)}>Log Button</Button>
     </Fragment>
   );
 };
