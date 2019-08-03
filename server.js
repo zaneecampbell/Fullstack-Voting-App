bae75a53e3fe823595c153cad6906d16677829c0
Zane Campbell <zanegitinit@gmail.com>
2019-08-02 21:24:13 -0700

    attempt 2 for production setup

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 connectDB();
 
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
+// Defined Route/s
 app.use('/api', require('./routes/api/poll'));
+
+// Server statis assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set statis folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
